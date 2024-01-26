f38299b6a76c640dbaff0d2a170711997b2fd54b
rahulrsavant <50170885+rahulrsavant@users.noreply.github.com>
2024-01-26 16:03:16 +0530

    all java files with property & pom
    all java files with property & pom

---
+++ src/main/java/com/mkyong/model/SimpleBook.java
@@ -0,0 +1,18 @@
+package com.mkyong.model;
+
+public class SimpleBook {
+    private String title;
+
+    public SimpleBook(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
+
